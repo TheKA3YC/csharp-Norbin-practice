60f215c515b84fb9ee2a9b204da33d58b49f5b8d
kostiagaponov <pomochnik3000@gmail.com>
2026-06-16 21:00:04 +0400

    solution structure

---
+++ Типы данных - Задание 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+static string CalculateCompoundInterestForYears(double initialDeposit, int years, double interestRate)
+{
+    var сompoundInterestForYears = new StringBuilder();
+    double growth = 1 + interestRate / 100;
+    for (var i = 1; i <= years; i++)
+    {
+        initialDeposit = initialDeposit * growth;
+        сompoundInterestForYears.AppendLine($"Год {i}: {Math.Round(initialDeposit, 2)} руб.");
+    }
+
+    return сompoundInterestForYears.ToString();
+}
+
+Console.WriteLine(CalculateCompoundInterestForYears(1000, 3, 10));
---
+++ Типы данных - Задание 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+static string BuildDiamant(int size)
+{
+    var diamatStruct = new StringBuilder();
+    int center = size / 2;
+    for (var i = 0; i < size; i++)
+    {
+        for (var j = 0; j < size; j++)
+        {
+            if (Math.Abs(i - center) + Math.Abs(j - center) == center)
+            {
+                diamatStruct.Append('X');
+            }
+            else
+            {
+                diamatStruct.Append(' ');
+            }
+        }
+        diamatStruct.AppendLine();
+    }
+    return diamatStruct.ToString();
+}
+
+
+Console.WriteLine(BuildDiamant(5));
